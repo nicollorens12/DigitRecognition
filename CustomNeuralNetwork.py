e78498f45fe86fe7349c7b1d2b22b5d6a81a12f3
Nico Llorens <nicollorenss@gmail.com>
2024-10-02 22:06:40 +0200

    First working version (83% accuracy)

--- CustomNeuralNetwork.py
+++ CustomNeuralNetwork.py
@@ -15,7 +15,10 @@ class CustomNeuralNetwork:
         L = len(layer_dims)  # Total number of layers
 
         for l in range(1, L):
-            params['W'+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
+            # The random values are multiplied by np.sqrt(2 / layer_dims[l-1]) to avoid vanishing/exploding gradients
+            # as *0.01 was vanishing the gradients
+            params['W'+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * np.sqrt(2 / layer_dims[l-1])
+            #params['W'+str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.01
             params['b'+str(l)] = np.zeros((layer_dims[l], 1))
         
         return params
@@ -30,37 +33,52 @@ class CustomNeuralNetwork:
         
         return A, cache
     
+    #ReLU is used in hidden layers of the network
+    #It is used to introduce non-linearity in the network
+    #It is computationally less expensive than sigmoid and tanh
+    def relu(self, Z):
+        return np.maximum(0, Z), Z
+    
     #Softmax function is used for multi-class classification problems
     #It is used in the output layer of the network to get the probabilities of each class as a total of 1
     def softmax(self, Z):
         expZ = np.exp(Z - np.max(Z, axis=0, keepdims=True)) 
-        return expZ / np.sum(expZ, axis=0, keepdims=True)
+        A = expZ / np.sum(expZ, axis=0, keepdims=True)
+        cache = Z  # Almacena Z como cache para la retropropagación
+        return A, cache
 
+    
+    
+    #Relu backward is used in backpropagation
+    #It is used to calculate the gradients of the weights and biases
+    def relu_backward(self, dA, cache):
+        Z = cache
+        dZ = np.array(dA, copy=True)  # Convertir dA en un array en lugar de una referencia
+        dZ[Z <= 0] = 0  # Gradiente es 0 para Z <= 0
+        return dZ
 
     #Takes traning data and parameters
     #Generates output for one layer and then it will feed that output to the next layer and so on.
     #Cache is needed for the future step 'backpropagation'
-    def forward_prop(self,X, params):
-        A = X # input to first layer i.e. training data
+    def forward_prop(self, X, params):
+        A = X  # input to the first layer
         caches = []
-        L = len(params)//2 #As each layer has W matrix and b array, L is the number of Layers of our network
+        L = len(params) // 2  # Number of layers (W and b for each layer)
 
-        for l in range(1,L+1):
+        for l in range(1, L+1):
             A_prev = A
+            Z = np.dot(params['W' + str(l)], A_prev) + params['b' + str(l)]
+            
+            if l == L:  # Last layer, use softmax
+                A, activation_cache = self.softmax(Z)
+            else:  # Hidden layers, use ReLU
+                A, activation_cache = self.relu(Z)
 
-            # Linear combination of weights, inputs and biases to get the resulting activation values (before sigmoid squeezification)
-            Z = np.dot(params['W'+str(l)], A_prev) + params['b'+str(l)]
-
-            # Storing the linear cache
-            linear_cache = (A_prev, params['W'+str(l)], params['b'+str(l)])
-
-            # Applying sigmoid on linear hypothesis
-            A, activation_cache = self.sigmoid(Z) #activation_cache is Z before sigmoid
-
-            cache = (linear_cache, activation_cache) 
+            # Save the caches for backpropagation
+            linear_cache = (A_prev, params['W' + str(l)], params['b' + str(l)])
+            cache = (linear_cache, activation_cache)
             caches.append(cache)
-        
-        A = self.softmax(A)
+
         return A, caches
 
 
@@ -73,18 +91,16 @@ class CustomNeuralNetwork:
 
 
 
-    def oneLayerBackward(self, dA, cache, activation):
+    def oneLayerBackward(self, dZ, cache, activation):
         linear_cache, activation_cache = cache
         A_prev, W, b = linear_cache
-        
-        if activation == "softmax":
-            dZ = dA  # dA es directamente dZ para softmax con entropía cruzada
-        else:
-            Z = activation_cache  # Recupera Z desde el activation_cache
-            A, _ = self.sigmoid(Z)
-            dZ = dA * A * (1 - A)  # Derivada de sigmoid
-        
         m = A_prev.shape[1]
+
+        if activation == "relu":
+            Z = activation_cache
+            dZ = np.array(dZ, copy=True)
+            dZ[Z <= 0] = 0  # ReLU derivative
+
         dW = np.dot(dZ, A_prev.T) / m
         db = np.sum(dZ, axis=1, keepdims=True) / m
         dA_prev = np.dot(W.T, dZ)
@@ -96,28 +112,28 @@ class CustomNeuralNetwork:
     #Backprop is the process of updating the weights and biases of the network to minimize the cost function
     #Pre: The activation values of the last layer of our NN, the real values and the caches
     #Post: Returns the gradients of the weights and biases
-    def backprop(self, AL, Y, caches):
+    def backprop(self, Y_hat, Y, caches):
         grads = {}
-        L = len(caches)  # Número de capas
-        m = AL.shape[1]
-        Y = Y.reshape(AL.shape)
+        L = len(caches)  # Number of layers
+        m = Y_hat.shape[1]
         
-        assert AL.shape == Y.shape, f"Dimensiones incompatibles: AL {AL.shape}, Y {Y.shape}"
+        Y = Y.reshape(Y_hat.shape)
+        
+        assert Y_hat.shape == Y.shape # Asegurar que las dimensiones son iguales
+        
+        # Calculate dZL for softmax (output layer)
+        dZL = Y_hat - Y
+        current_cache = caches[L-1]
+        grads['dA' + str(L-1)], grads['dW' + str(L)], grads['db' + str(L)] = self.oneLayerBackward(dZL, current_cache, activation="softmax")
 
-        # Derivada del costo con respecto a la salida de la última capa
-        dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
 
         # Calcular los gradientes para la última capa (usando sigmoid en este ejemplo)
-        current_cache = caches[L-1]
-        grads['dA' + str(L-1)], grads['dW' + str(L)], grads['db' + str(L)] = self.oneLayerBackward(dAL, current_cache, activation="sigmoid")
-
-        # Calcular los gradientes para las capas anteriores (usando relu en este ejemplo)
         for l in reversed(range(L-1)):
             current_cache = caches[l]
-            dA_prev_temp, dW_temp, db_temp = self.oneLayerBackward(grads["dA" + str(l+1)], current_cache, activation="relu")
-            grads["dA" + str(l)] = dA_prev_temp
-            grads["dW" + str(l+1)] = dW_temp
-            grads["db" + str(l+1)] = db_temp
+            dA_prev_temp, dW_temp, db_temp = self.oneLayerBackward(grads['dA' + str(l+1)], current_cache, activation="relu")
+            grads['dA' + str(l)] = dA_prev_temp
+            grads['dW' + str(l+1)] = dW_temp
+            grads['db' + str(l+1)] = db_temp
 
         return grads
 
@@ -144,16 +160,16 @@ class CustomNeuralNetwork:
     #Post: Returns the parameters of the network and the cost history
     # Epochs is a hyperparameter that indicates how many times the network will see the training data
     # (the more epochs, the more the network will learn but it can also overfit the data)
-    def train(self, X, Y, layer_dims, epochs, lr):
+    def train(self, X, Y, layer_dims):
         params = self.initParams(layer_dims)
         cost_history = []
 
-        for i in range(epochs):
+        for i in range(self.epochs):
             Y_hat, caches = self.forward_prop(X, params)
             cost = self.cost_function(Y_hat, Y)
             cost_history.append(cost)
             grads = self.backprop(Y_hat, Y, caches)
-            params = self.update_parameters(params, grads, lr)
+            params = self.update_parameters(params, grads, self.lr)
 
         return params, cost_history
     
